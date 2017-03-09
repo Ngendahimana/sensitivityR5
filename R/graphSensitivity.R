38525d8894786d6cfeff0a6f6da054e1ee5ef40c
Ngendahimana <dkn18@case.edu>
2017-03-09 12:38:28 -0500

    restructing package

---
+++ R/graphSensitivity.R
@@ -0,0 +1,69 @@
+
+#' Visualizing sensivity results
+#'
+#' @param x  Count of pairs where only control has outcome.
+#' @param y  Count of pairs where onlytreated has outcome.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' binSensgraph (1, 1)
+#' binsSensgraph (10, 1)
+
+binSensgraph = function (x, y = NULL, Gamma = 3, GammaInc = .2,alpha = 0.06)
+{
+  if (length(x) == 1) { # in case your are passing # of discocordant pairs
+    ctrl <- x
+    trt <- y
+  }
+  else { # in case you are passing an object from another function
+    y.c <- x$mdata$Y[x$mdata$Tr == 0] # select outcome for controls
+    y.t <- x$mdata$Y[x$mdata$Tr == 1] #select outcomes for treated
+    table(y.t, y.c)
+    y.tmp1 <- table(y.t, y.c)[2] # select 1st set of  discocordant pairs i.e treated had outcome, control did not hav outcome
+    y.tmp2 <- table(y.t, y.c)[3] # select 2nd set of  discocordant pairs i.e control had outcome, treated did not have outcome
+    (if (y.tmp1 >= y.tmp2) {
+      trt <- y.tmp1
+      ctrl <- y.tmp2
+    }                          # code to ensure # treated is always greater than controlls
+      else {
+        trt <- y.tmp2
+        ctrl <- y.tmp1
+      })
+  }
+  gamma <- seq(1, Gamma, by = GammaInc)
+  mx <- ctrl + trt
+  up <- c() # creating an empty vector to store upper bound
+  lo <- c() # creating an empty vector to store lower bound
+  series <- seq(trt, mx, by = 1)
+  n.it <- length(gamma)
+  for (i in 1:n.it) {
+    p.plus <- gamma[i]/(1 + gamma[i])
+    p.minus <- 1/(1 + gamma[i])
+    up.tmp <- sum(dbinom(series, mx, prob = p.plus))
+    lo.tmp <- sum(dbinom(series, mx, prob = p.minus))
+    up <- c(up, up.tmp)
+    lo <- c(lo, lo.tmp)
+  }
+  pval <- lo[1]
+  bounds <- data.frame(gamma, plower = round(lo, 5), pupper = round(up, 5))
+
+  bounds$min = abs(alpha - bounds$pupper)
+
+  vrt = round(bounds[bounds$min == min(bounds$min), ]$gamma,2)
+  hrz = round(bounds[bounds$min == min(bounds$min), ]$pupper,2)
+
+
+  #ggplot(data = bounds, aes(x = gamma,y = pupper))+geom_line()+geom_point(aes(x=vrt,y=hrz))+ylab("p upper bound")+xlab("gamma (Bias)")+theme_bw()+annotate("text",x=vrt,y=hrz,hjust=1.3,label=paste0("(",vrt,",",hrz,")"))+labs(title = "Binary Outcome Sensitivity Plot")
+
+  plot(bounds$pupper ~ bounds$gamma, type = "l", xlab = "Gamma", ylab = "p-val upper bound", main = "Sensitivity plot for binary outcomes")
+  text(vrt,hrz,paste0("(",vrt,",",hrz,")"),pos = 2)
+  points(vrt,hrz,pch=15)
+
+
+  colnames(bounds) <- c("Gamma", "Lower bound", "Upper bound")
+  msg <- "Rosenbaum Sensitivity Test \n"
+  note <- "Note: Gamma is Odds of Differential Assignment To\n Treatment Due to Unobserved Factors \n"
+  Obj <- list(Gamma = Gamma, GammaInc = GammaInc, pval = pval,
+              msg = msg, bounds = bounds, note = note)
+  class(Obj) <- c("rbounds", class(Obj))
+  Obj
+}
