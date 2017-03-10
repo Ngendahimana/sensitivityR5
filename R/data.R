bd70fdb17f07a7a064b0fc1b64bb9738dc29a394
Ngendahimana <dkn18@case.edu>
2017-03-09 20:25:59 -0500

    documenting

--- R/data.R
+++ R/data.R
@@ -22,6 +22,15 @@
 #' The Data Set is 100% fictional, and is available as toy.csv on the course website. It #' 70 treated - subjects 131-200 and 130 controls -     #' subjects 1-130  on treatment status. It has three outcomes, with no    #' missing observations anywhere. We assume that a logical argument suggests that the   #' square of covA, as well as the interactions of covB with covC and with covD should be #' related to treatment assignment, and thus should be included in our propensity model.
 #'
 #' @format A data frame with 200 rows and 6 variables:
+#' #' \describe{
+#'   \item{Age}{Age, in years}
+#'   \item{Sex}{Sex at birth}
+#'   \item{Income}{Yearly Income, in dollars}
+#'   \item{meta}{Metabolic Diagnostic}
+#'   .
+#'   .
+#'   .
+#' }
 #' @source \url{https://sites.google.com/a/case.edu/love-500/home/data-and-code}
 "toy"
 
