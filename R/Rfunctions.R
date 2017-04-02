d9c16084a6a2d6130187ac51cae4d03e798af522
Ngendahimana <dkn18@case.edu>
2017-04-02 19:08:07 -0400

    clean up

--- R/Rfunctions.R
+++ R/Rfunctions.R
@@ -346,6 +346,8 @@ Survsens = function(x, y=NULL,data =NULL, exp=NULL, outcome=NULL, failtime, Gamm
         bounds$plower[i] = round(min(1, 2 * pnorm((expoutlive - bounds[i, 4])/bounds[i, 6], lower.tail = FALSE)), 4)
     }
 
+    bounds2 = bounds[,c(1,8,7)]
+    names(bound2)=c("Gamma","Lower bound"," Upper bound")
     bounds1 = bounds
 
     bounds1$min = abs(alpha - bounds1$pupper)
