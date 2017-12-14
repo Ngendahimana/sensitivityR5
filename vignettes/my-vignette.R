d65ecf1a44c1d1d835c4cb9c2be1f1f150caf312
KwitondaNgenda <d_kwitonda@yahoo.com>
2017-12-14 09:51:15 -0500

    update

---
+++ vignettes/my-vignette.R
@@ -0,0 +1,12 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+#library(tidyverse);library(tidyr)
+
+## ----packageinstall,message=FALSE----------------------------------------
+
+devtools::install_github("Ngendahimana/SensitivityR5")
+
+
