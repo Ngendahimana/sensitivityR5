4a44feb6aa971272d7142a88ddf134c352cd096c
KwitondaNgenda <d_kwitonda@yahoo.com>
2018-02-01 13:13:52 -0500

    update

--- inst/doc/my-vignette.R
+++ inst/doc/my-vignette.R
@@ -180,3 +180,57 @@ mod = lm(VOTED98 ~ PHN.C1+PERSONS + VOTE96.1 + NEW +MAJORPTY + AGE + WARD,data =
 binarysens2(x=m.out,y ="VOTED98", Gamma=2, GammaInc=.1)
 
 
+## ----match1_matchit------------------------------------------------------
+data("lalonde",package = "Matching")
+m.out = matchit(treat ~ age  + educ +  black + hisp +married + nodegr + re74  + re75  +
+u74 + u75, family=binomial, data = lalonde, method = "nearest")
+
+
+## ----sens_cont-----------------------------------------------------------
+mod = lm(re78~age  + educ +  black + hisp +married + nodegr + re74  + re75  +u74 + u75,data = match.data(m.out))
+
+sens_object = pens2(x = m.out, y="re78",Gamma = 2, GammaInc = 0.1,est = 629.7)
+
+
+## ----sens_cont_results---------------------------------------------------
+
+kable(sens_object$bounds)
+
+## ----designMatch---------------------------------------------------------
+
+data("lalonde",package = "designmatch")
+library(designmatch)
+attach(lalonde)
+## Treatment indicator
+t_ind = treatment
+## Distance matrix
+dist_mat = NULL
+## Subset matching weight
+subset_weight = 1
+## Moment balance: constrain differences in means to be at most .05 standard deviations apart
+mom_covs = cbind(age, education, black, hispanic, married, nodegree, re74, re75)
+mom_tols = round(absstddif(mom_covs, t_ind, .05), 2)
+mom = list(covs = mom_covs, tols = mom_tols)
+## Fine balance
+fine_covs = cbind(black, hispanic, married, nodegree)
+fine = list(covs = fine_covs)
+## Exact matching
+exact_covs = cbind(black)
+exact = list(covs = exact_covs)
+## Solver options
+t_max = 60*5
+solver = "glpk"
+approximate = 1
+solver = list(name = solver, t_max = t_max, approximate = approximate,round_cplex = 0, trace = 0)
+## Match
+out = bmatch(t_ind = t_ind, dist_mat = dist_mat, subset_weight = subset_weight,mom = mom, fine = fine, exact = exact, solver = solver)
+
+
+## ----pens2_cardMatch-----------------------------------------------------
+
+cardMatch = pens2(x = out, y="re78",Gamma = 2, GammaInc = 0.1,est = 234,treat = "treatment",data = lalonde)
+
+kable(head(cardMatch$bounds,5))
+detach(lalonde)
+
+
